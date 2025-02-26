79a2cd1a9dbe7d47582be4c2af73c526b4b64df7
Aizat <aayzat2004@gmail.com>
2025-02-26 21:45:23 +0300

    док файл +

--- to_audio.py
+++ to_audio.py
@@ -6,7 +6,8 @@ import pyttsx3
 def say_text(text):
     # Инициализация движка
     engine = pyttsx3.init()
-
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)  # Измените индекс
     # Установка параметров
     engine.setProperty('rate', 150)  # Скорость речи
     engine.setProperty('volume', 1)  # Громкость (от 0.0 до 1.0)
@@ -14,3 +15,4 @@ def say_text(text):
     # Озвучивание текста
     engine.say(text)
     engine.runAndWait()
+
