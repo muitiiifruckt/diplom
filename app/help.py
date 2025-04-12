408a0ffbc560d160d1c35dac116143e274d04ab8
Aizat <aayzat2004@gmail.com>
2025-04-12 20:39:07 +0300

    правильный формат wav отпрравляется на бэк

--- app/help.py
+++ app/help.py
@@ -1,7 +1,7 @@
 from vosk import Model, KaldiRecognizer
 import pyaudio
 import json
-
+import wave
 def recognize_speech(model_path="vosk-model-small-en-us-0.15", sample_rate=16000):
     # Инициализация модели и распознавателя
     model = Model("vosk-model-small-en-us-0.15")
@@ -21,3 +21,28 @@ def recognize_speech(model_path="vosk-model-small-en-us-0.15", sample_rate=16000
             text = result.get("text", "")
             print("Распознанный текст:", text)
             return text
+        
+def recognize_speech_from_wav(wav_path, model_path="vosk-model-small-en-us-0.15"):
+    # Инициализация модели
+    model = Model("vosk-model-small-en-us-0.15")
+    
+    # Открытие WAV-файла
+    with wave.open(wav_path, "rb") as wf:
+        sample_rate = wf.getframerate()
+        
+        # Инициализация распознавателя с параметрами файла
+        recognizer = KaldiRecognizer(model, sample_rate)
+        
+        # Чтение и обработка данных
+        while True:
+            data = wf.readframes(4096)
+            if len(data) == 0:
+                break
+            if recognizer.AcceptWaveform(data):
+                pass  # Промежуточные результаты можно обрабатывать здесь
+        
+        # Получение финального результата
+        result = json.loads(recognizer.FinalResult())
+        return result.get("text", "")
+if __name__ ==("__main__"):
+    print(recognize_speech_from_wav(r"recording.wav"))
